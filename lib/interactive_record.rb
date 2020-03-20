a913098456c09edc5f76d4f1a3ec022d8d49813b
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-20 02:41:03 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -12,7 +12,7 @@ class InteractiveRecord
     sql= "pragma table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each do |col|
+    table_info.each do |row|
       column_names << row["name"]
     end
   end
