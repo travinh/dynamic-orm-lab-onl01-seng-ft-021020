dc5b00e3ecdb7504a4d90c679f7cf95cb83d926e
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-20 02:50:39 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,4 +40,20 @@ class InteractiveRecord
     values.join(", ")
   end
   
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.join} = '#{attribute.values.first}'"
+    DB[:conn].execute(sql)
+  end
+  
 end
